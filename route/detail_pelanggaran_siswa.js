cc158f2b05e9cc92a0f2c14d27c5a72851bf443e
azkiyaputri <58657776+azkiyaputri@users.noreply.github.com>
2021-04-28 16:28:38 +0700

    Update detail_pelanggaran_siswa.js

--- route/detail_pelanggaran_siswa.js
+++ route/detail_pelanggaran_siswa.js
@@ -8,9 +8,9 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-// GET: /detail_pelanggaran_siswa/:id_pelanggaran_siswa --> end-point untuk menampilkan detail pelanggaran
-app.get("/:id_pelanggaran_siswa", (req,res) => {
-    let param = { id_pelanggaran_siswa: req.params.id_pelanggaran_siswa}
+// GET: /detail_pelanggaran_siswa/:id_pelanggarannya_siswa --> end-point untuk menampilkan detail pelanggaran
+app.get("/:id_pelanggarannya_siswa", (req,res) => {
+    let param = { id_pelanggarannya_siswa: req.params.id_pelanggarannya_siswa}
 
     // create sql query
     let sql = "select * from detail_pelanggaran_siswa dps join pelanggaran p "+
@@ -30,7 +30,7 @@ app.get("/:id_pelanggaran_siswa", (req,res) => {
 // POST: /detail_pelanggaran_siswa --> end point untuk menambahkan detail_pelanggaran
 app.post("/", (req,res) => {
     let data = {
-        id_pelanggaran_siswa: req.body.id_pelanggaran_siswa,
+        id_pelanggarannya_siswa: req.body.id_pelanggarannya_siswa,
         id_pelanggaran: req.body.id_pelanggaran
     }
     let message = ""
@@ -51,14 +51,14 @@ app.post("/", (req,res) => {
     })
 })
 // end-point untuk menghapus data detail_pelanggaran_siswa
-app.delete("/:id_pelanggaran_siswa/:id_pelanggaran", (req, res) => {
+app.delete("/:id_pelanggarannya_siswa/:id_pelanggaran", (req, res) => {
     let data = [
-        req.params.id_pelanggaran_siswa,
+        req.params.id_pelanggarannya_siswa,
         req.params.id_pelanggaran
     ]
 
     // create sql query delete detail_pelanggaran
-    let sql = "delete from detail_pelanggaran_siswa where id_pelanggaran_siswa = ? and id_pelanggaran = ?"
+    let sql = "delete from detail_pelanggaran_siswa where id_pelanggarannya_siswa = ? and id_pelanggaran = ?"
 
     db.query(sql, data, (error, result) => {
         if (error) {
